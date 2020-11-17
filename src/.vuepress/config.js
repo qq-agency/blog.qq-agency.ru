3bf7526ff716928778fb8e333dfe30b1f54ac663
Sergey Panteleev <sergey@php.net>
2020-11-17 12:29:52 +0300

    feat: Яндекс.Метрика
    - Добавление Яндекс.Метрики

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -37,5 +37,17 @@ module.exports = {
   plugins: [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
+    [
+      'metrika',
+      {
+        counter: '69476659',
+        config: {
+          accurateTrackBounce: true,
+          clickmap: true,
+          trackLinks: true,
+          webvisor: true,
+        },
+      },
+    ],
   ],
 };
